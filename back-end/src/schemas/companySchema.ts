9353a0508247cf516108b7a4a9104f1916b562a9
Icaro-pavani <icaro.pavani79@gmail.com>
2022-10-09 19:06:10 -0300

    fix: added pattern for schemas in cpf and cnpj

--- back-end/src/schemas/companySchema.ts
+++ back-end/src/schemas/companySchema.ts
@@ -5,6 +5,8 @@ export type CreateCompanyParams = Omit<Company, "_id">;
 
 export const createCompanySchema = Joi.object<CreateCompanyParams>({
   name: Joi.string().required(),
-  cnpj: Joi.string().required(),
+  cnpj: Joi.string()
+    .pattern(/^\d{2}.\d{3}.\d{3}\/\d{4}-\d{2}$/)
+    .required(),
   password: Joi.string().min(6).required(),
 });
--- back-end/src/schemas/userSchemas.ts
+++ back-end/src/schemas/userSchemas.ts
@@ -7,11 +7,15 @@ export type SignInUserParams = Omit<CreateUserParams, "name">;
 
 export const createUserSchema = Joi.object<CreateUserParams>({
   name: Joi.string().required(),
-  cpf: Joi.string().required(),
+  cpf: Joi.string()
+    .pattern(/^\d{3}.\d{3}.\d{3}-\d{2}$/)
+    .required(),
   password: Joi.string().min(6).required(),
 });
 
 export const singInUserSchema = Joi.object<SignInUserParams>({
-  cpf: Joi.string().required(),
+  cpf: Joi.string()
+    .pattern(/^\d{3}.\d{3}.\d{3}-\d{2}$/)
+    .required(),
   password: Joi.string().required(),
 });
